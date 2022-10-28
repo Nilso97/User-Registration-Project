08e3ec129acbd2c8ba1b5812104d70b13c811770
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-10-28 06:31:36 -0300

    atualizando a aplicação springboot

--- src/main/java/br/com/springboot/springboot/repository/UserRepository.java
+++ src/main/java/br/com/springboot/springboot/repository/UserRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import br.com.springboot.springboot.models.User;
+import br.com.springboot.springboot.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Query("SELECT user FROM User user WHERE user.id > :id")
+    
+    @Query("SELECT u FROM User u WHERE u.id > :id")
     public List<User> findAllMoreThan(@Param("id") Long id);
 
     public List<User> findByIdGreaterThan(Long id);
